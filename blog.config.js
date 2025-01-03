aac0506197f662cdc4ffcb76effb0662e828f506
xsoway <139938648+xsoway@users.noreply.github.com>
2025-01-03 20:45:18 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -60,26 +60,45 @@ const BLOG = {
   // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
   // 后面空格隔开的font-light的字体粗细，留空是默认粗细；参考 https://www.tailwindcss.cn/docs/font-weight
   FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans font-light',
+  // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
   FONT_URL: [
-    'https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Lora:wght@400;700&family=Open+Sans:wght@300;400;700&display=swap',
+    // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
+    'https://fonts.googleapis.com/css?family=Bitter&display=swap',
+    'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
+    'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
   ],
+  // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
-    '"Open Sans"',
+    // '"LXGW WenKai"',
+    '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
+    '"Hiragino Sans GB"',
+    '"Microsoft YaHei"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
     '"Segoe UI"',
+    '"Noto Sans SC"',
+    'HarmonyOS_Regular',
     '"Helvetica Neue"',
+    'Helvetica',
+    '"Source Han Sans SC"',
     'Arial',
     'sans-serif',
-    '"Apple Color Emoji"',
+    '"Apple Color Emoji"'
   ],
+  // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
-    '"Merriweather"',
-    '"Lora"',
-    'Georgia',
+    // '"LXGW WenKai"',
+    'Bitter',
+    '"Noto Serif SC"',
+    'SimSun',
     '"Times New Roman"',
+    'Times',
     'serif',
-    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+    '"Apple Color Emoji"'
   ],
   FONT_AWESOME:
     process.env.NEXT_PUBLIC_FONT_AWESOME_PATH ||
