3523738089f82f0020b6460ef38ce39afc8e09eb
xsoway <139938648+xsoway@users.noreply.github.com>
2025-01-03 21:28:25 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -15,14 +15,14 @@ const CONFIG = {
   HEXO_SHOW_START_READING: true,
 
   // 菜单配置
-  HEXO_MENU_INDEX: false, // 显示首页
+  HEXO_MENU_INDEX: true, // 显示首页
   HEXO_MENU_CATEGORY: true, // 显示分类
   HEXO_MENU_TAG: true, // 显示标签
   HEXO_MENU_ARCHIVE: true, // 显示归档
   HEXO_MENU_SEARCH: true, // 显示搜索
   HEXO_MENU_RANDOM: true, // 显示随机跳转按钮
 
-  HEXO_POST_LIST_COVER: false, // 列表显示文章封面
+  HEXO_POST_LIST_COVER: true, // 列表显示文章封面
   HEXO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
 
   HEXO_POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
