7e8a8bb8b1008347e711859889a6a2164a22183e
xsoway <139938648+xsoway@users.noreply.github.com>
2025-02-07 09:56:05 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Alan_Hsu', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '许心志我在 蓝湛阔天海 中定自主宰🍚', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '许心志我在 蓝湛阔天海 中定自主宰', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://xmylog.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 随感,技术,经验,旅行,推荐,生活,音乐,电影 etc.', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '离开乏味的皮囊，自由的灵魂在路上',
+    '离开乏味的皮囊，自由的灵魂在路上,欢迎来到我的博客🎉',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
